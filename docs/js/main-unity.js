b76b625be7dabe09a29dfcace800fda0bc88978a
mChopper <mgs.milton@gmail.com>
2022-12-14 16:40:00 -0300

    Update main-unity.js

--- docs/js/main-unity.js
+++ docs/js/main-unity.js
@@ -1,5 +1,5 @@
 var buildUrl = "../docs/Build/";
-var builString = "Xplay"
+var builString = "PalestraUniftec"
 var loaderUrl = buildUrl + builString + ".loader.js";
 var config = {
 //     dataUrl: "https://agoravirtual-bucket.s3.us-west-2.amazonaws.com/WebGL_Pedro.data",
